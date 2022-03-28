360333fc321300a6f8c9b8c14c0a4ea65b0bb6ba
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-03-28 10:44:46 -0500

    add metadata

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,7 +6,10 @@ import Footer from "../components/footer";
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<div className="d-flex flex-column justify-content-between" style={{minHeight: "100vh"}}>
+		<div
+			className="d-flex flex-column justify-content-between"
+			style={{ minHeight: "100vh" }}
+		>
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta name="theme-color" content="#000000" />
@@ -16,10 +19,20 @@ function MyApp({ Component, pageProps }) {
 					name="description"
 					content="Professional Repair Service for Apple and Samsung Products"
 				/>
+				<meta
+					name="keywords"
+					content="broken glass, broken screens, broken lcd, tablet repair, phone repair, lcd buyback, lcd swap, iwatch repair, apple watch, replacement, replace, mail in repair"
+				/>
+				<meta property="og:title" content="Lenwell" />
+				<meta property="og:image" content="/bg-home2.jpg" />
+				<meta
+					property="og:description "
+					content="Lenwell Digital is a Samsung and Apple mobile device repair shop specializing in the repair of screen glass panel, touchpad, LCD. Expert technicians will fix your cracked/broken iPhone, iPad, iWatch, or Samsung phone and tablet with the best genuine parts. Lenwell Digital is the place to go for your mobile device repair needs."
+				/>
 			</Head>
-			<NavBar/>
+			<NavBar />
 			<Component {...pageProps} />
-			<Footer/>
+			<Footer />
 		</div>
 	);
 }
