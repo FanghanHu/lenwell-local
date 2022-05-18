640b6a6d71e900b036f745c88fdd34498d54a9f2
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-05-18 18:14:37 -0500

    updated referrer page

--- pages/referrer.jsx
+++ pages/referrer.jsx
@@ -11,64 +11,69 @@ export default function Referrer() {
 					height: "350px",
 				}}
 			></div>
-			<div className="container text-center">
-				<div className="p-3">
-					As a referrer, you can earn some extra money and enjoy a discounted
-					price on all our services. The amount you can earn depends on your
-					negotiation skills, the better you can bargain, the more you will
-					earn.
-				</div>
-				<div className="h3 text-center mt-5 border-bottom p-1">
-					How to start
-				</div>
-				<div className="p-3">
-					Come to the store to set up your account and get an orientation.
-				</div>
-				<div
-					className="mx-auto"
-					style={{ height: "30px", width: "0", borderRight: "1px solid gray" }}
-				></div>
-				<div className="p-3">
-					Go to the <Link href="/repair-service">price list</Link> and use your
-					account number to figure out your costs
-				</div>
-				<div
-					className="mx-auto"
-					style={{ height: "30px", width: "0", borderRight: "1px solid gray" }}
-				></div>
-				<div className="p-3">
-					Find customers and bargain for deals, the more customer you can find,
-					the lower you cost will become
-				</div>
-
-				<div className="h5 mt-4">Direct a customer to the store</div>
-				<div className="p-3">
-					If you found a customer on the phone, or via internet, you may ask
-					your customer to drop their device and pay us directly, you will get
-					your earnings at the end of day via Paypal or CashApp.
-				</div>
-				<div className="p-3">
-					To keep track of your customers you need to{" "}
-					<Link href="/order-online">create a ticket</Link> for your customer,
-					remember to enter your account, and the price your customer agreed on.
-					give your customer the generated ticket number, and instruct them to
-					show us the ticket number when they arrive at the store.
-				</div>
-
-				<div className="h5 mt-4">Deliver the device yourself</div>
-				<div className="p-3">
-					You can provide a better service by picking up the device from your
-					customers and delivering it back after it is repaired.
-				</div>
-				<div className="p-3">
-					In this case you do not need to create a ticket online, we will create
-					the ticket for you when you drop off the device
-				</div>
-				<div className="text-center m-5">
-					<div className="h5">Have a question?</div>
-					<Link href="/contact-us">
-						<a className="button button-primary px-3 py-1 h5">Contact us</a>
-					</Link>
+			<div className="container py-5">
+				<div className="row">
+					<div className="col-3">
+						<ol>
+							<li>
+								<a href="#introduction">Introduction</a>
+							</li>
+							<li>
+								<a href="#getting-started">Getting Started</a>
+							</li>
+							<li>
+								<a href="#price">How much can I earn?</a>
+							</li>
+							<li>
+								<a href="#order">Submit an order</a>
+							</li>
+							<li>
+								<a href="#get-paid">How to get paid</a>
+							</li>
+						</ol>
+					</div>
+					<div className="col-9">
+						<div id="introduction">
+							<div className="display-6">
+								Introduction
+							</div>
+							<p>
+								If your business involves phones or tablets, you are likely to get calls about repairing broken devices, and that is business opportunity, if you do not repair these devices yourself, consider send your client to us, and get paid in the process. 
+							</p>
+						</div>
+						<div id="getting-started">
+							<div className="display-6">
+								Getting Started
+							</div>
+							<p>
+								Before you can go out there and strike a deal, you need to register an account with us. With an account, you will have access to our deeply discounted business price list. Either contact in person, call us, or email us to get registered. we will need your business name, contact name, and phone number. You also need to fill out a tax exemption form. which can be downloaded <a href="/tax-exemption.pdf">here</a> .
+							</p>
+						</div>
+						<div id="price">
+							<div className="display-6">
+								How much can I earn?
+							</div>
+							<p>
+								Once you have your account ready, go to <Link href="/repair-service">Price List</Link> to see a complete list of services we provide and their prices, by default you will only see the retail price, but if you enter your account name or phone number to the web page, the page will then show your specific price list. Use this price list as a guide when you negotiate with your client. What you can bargin with your client minus the service cost from the price list is what you earn.
+							</p>
+						</div>
+						<div id="order">
+							<div className="display-6">
+								Submit an order
+							</div>
+							<p>
+								Fill out the form <Link href={'/order-online'}>Here</Link>, let us know what device and service to expect, use your own information for the contact, make sure you use the same phone number as the one you used to register your account, leave the price you bargained with your client and anything else we should know in the  description field. After you submit the order, you will get a ticket number, give your customer this ticket number and ask them to give this number to us when they show up in our store.
+							</p>
+						</div>
+						<div id="get-paid">
+							<div className="display-6">
+								How to get paid
+							</div>
+							<p>
+								We will charge your customer the full amount you bargained for, and then pay you your earnings. once your send your customer on their way, you can create a money request on Paypal or Cash App to collect your earnings. We will process your request at the end of day, or if the customer doesn't show up in a couple of days, the request will be denied.
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
